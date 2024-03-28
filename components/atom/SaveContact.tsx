f7c4b67373eff781b44bb520099b78ac5146083a
abdallahmoubarak <abdallah.moubarak02@gmail.com>
2024-03-28 04:27:37 +0200

    updating SaveContact component

---
+++ components/atom/SaveContact.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+import { useState } from "react";
+import { FaDownload } from "react-icons/fa";
+
+export default function SaveContact() {
+  const [isVisible, setIsVisible] = useState(true);
+  return (
+    <Link
+      className={`${isVisible ? "opacity-100" : "opacity-0"}`}
+      href={isVisible ? "/data/AbdallahMoubarak.vcf" : "#"}>
+      <div
+        onClick={() => setIsVisible(false)}
+        className={`fixed flex justify-center gap-4 w-full bottom-0 py-2 bg-sky-600 text-white
+          ${isVisible ? "opacity-100" : "opacity-0"}`}>
+        <FaDownload />
+        <span>Save Contact</span>
+      </div>
+    </Link>
+  );
+}
--- components/pages/home.tsx
+++ components/pages/home.tsx
@@ -3,24 +3,14 @@ import Brands from "@/components/section/Brands";
 import Hero from "@/components/section/Hero";
 import Promote from "@/components/section/Hero/Promote";
 import TopBar from "@/components/section/TopBar";
-import Button from "../atom/Button";
-import Link from "next/link";
-import { FaDownload } from "react-icons/fa";
+import SaveContact from "../atom/SaveContact";
 
 export default function Home() {
   return (
     <div>
       <TopBar active={"Home"} />
       <Hero />
-      <Link href={"/data/AbdallahMoubarak.vcf"}>
-        <div
-          className={
-            "fixed flex justify-center gap-4 w-full bottom-0 py-3 bg-sky-600 text-white"
-          }>
-          <FaDownload />
-          <span>Save Contact</span>
-        </div>
-      </Link>
+      <SaveContact />
       <Promote />
       <Bio />
       <Brands />
